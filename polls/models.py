5cccd68dfdfe61a60b7b972e979e06ecbb985af9
meisun0107 <0107sun.mei@gmail.com>
2022-03-07 00:15:06 -0500

    format fix

--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 
 from django.contrib import admin
 
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
